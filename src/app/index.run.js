72ab6ceb668c091d36d3a7c51a8fa05e9c3bb9e1
jeevadeepan <jeevacse89@gmail.com>
2017-11-18 17:13:45 -0800

    Added auth token to overcome git api rate limiting

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -1,4 +1,5 @@
-export function runBlock ($log) {
-  'ngInject';
-  $log.debug('runBlock end');
+export function runBlock($log, $http) {
+    'ngInject';
+    $log.debug('runBlock end');
+    $http.defaults.headers.common.Authorization = 'token 70f02798a8acfc6af028d8eebe167b5a5d74bdd1';
 }
