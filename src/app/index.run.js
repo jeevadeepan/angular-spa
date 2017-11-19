08cec3583a6e1a257b4f85a57061d5218fcdf181
jeevadeepan <jeevacse89@gmail.com>
2017-11-18 17:19:00 -0800

    updated token

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -1,5 +1,5 @@
 export function runBlock($log, $http) {
     'ngInject';
     $log.debug('runBlock end');
-    $http.defaults.headers.common.Authorization = 'token 70f02798a8acfc6af028d8eebe167b5a5d74bdd1';
+    $http.defaults.headers.common.Authorization = 'token c53bb7f7cb6e9486e12153ee5b9363cd61f6f7b3';
 }
