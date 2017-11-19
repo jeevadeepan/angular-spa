84e258baf0684f573cc98c78aa42d3e553c111e8
jeevadeepan <jeevacse89@gmail.com>
2017-11-18 17:22:48 -0800

    Removed token

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -1,5 +1,5 @@
-export function runBlock($log, $http) {
+export function runBlock($log) {
     'ngInject';
     $log.debug('runBlock end');
-    $http.defaults.headers.common.Authorization = 'token c53bb7f7cb6e9486e12153ee5b9363cd61f6f7b3';
+//    $http.defaults.headers.common.Authorization = 'token c53bb7f7cb6e9486e12153ee5b9363cd61f6f7b3';
 }
